9e347f58dc65361562d1a17cd044454de618df9d
Ben <benjamincjackson@gmail.com>
2022-04-11 13:09:06 +0100

    adds a test for --table

--- pkg/updown/topranking_test.go
+++ pkg/updown/topranking_test.go
@@ -672,3 +672,101 @@ Query1,TargetSame1_dist0,TargetUp2_dist1;TargetUp1_dist2,TargetDown1_dist1,Targe
 		t.Errorf("problem in TestTopRankingDist3(csv)")
 	}
 }
+
+func TestTopRankingTable1(t *testing.T) {
+	refData := []byte(`>ref
+ATGATG
+`)
+	queryData := []byte(
+		`>Query1
+ATTATT
+`)
+
+	targetData := []byte(`>TargetUp1
+ATGATG
+>TargetSame1
+ATTATT
+>TargetDown1
+ATTACT
+>TargetUp2
+ATGATT
+>TargetSide1
+CCCCCC
+>TargetSide2
+ATGCTT
+`)
+	ref := bytes.NewReader(refData)
+	query := bytes.NewReader(queryData)
+	target := bytes.NewReader(targetData)
+	out := new(bytes.Buffer)
+	table := true
+	qtype := "fasta"
+	ttype := "fasta"
+	ignoreArray := make([]string, 0)
+	TRsizetotal := 5
+	TRsizeup := 0
+	TRsizedown := 0
+	TRsizeside := 0
+	TRsizesame := 0
+	TRdistall := 0
+	TRdistup := 0
+	TRdistdown := 0
+	TRdistside := 0
+	TRthresholdpair := float32(0.1)
+	TRthresholdtarget := 10000
+	TRnofill := false
+	TRdistpush := 0
+
+	err := TopRanking(query, target, ref, out, table,
+		qtype, ttype, ignoreArray,
+		TRsizetotal, TRsizeup, TRsizedown, TRsizeside, TRsizesame,
+		TRdistall, TRdistup, TRdistdown, TRdistside,
+		TRthresholdpair, TRthresholdtarget, TRnofill, TRdistpush)
+	if err != nil {
+		t.Error(err)
+	}
+
+	desiredResult := `query,direction,distance,target
+Query1,same,0,TargetSame1
+Query1,up,1,TargetUp2
+Query1,up,2,TargetUp1
+Query1,down,1,TargetDown1
+Query1,side,2,TargetSide2
+`
+	if string(out.Bytes()) != desiredResult {
+		t.Errorf("problem in TestTopRankingTable1(fasta)")
+	}
+
+	ref = bytes.NewReader(refData)
+	query = bytes.NewReader(queryData)
+	queryList := new(bytes.Buffer)
+	err = List(ref, query, queryList)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ref = bytes.NewReader(refData)
+	target = bytes.NewReader(targetData)
+	targetList := new(bytes.Buffer)
+	err = List(ref, target, targetList)
+	if err != nil {
+		t.Error(err)
+	}
+
+	qtype = "csv"
+	ttype = "csv"
+	out = new(bytes.Buffer)
+
+	err = TopRanking(queryList, targetList, ref, out, table,
+		qtype, ttype, ignoreArray,
+		TRsizetotal, TRsizeup, TRsizedown, TRsizeside, TRsizesame,
+		TRdistall, TRdistup, TRdistdown, TRdistside,
+		TRthresholdpair, TRthresholdtarget, TRnofill, TRdistpush)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(out.Bytes()) != desiredResult {
+		t.Errorf("problem in TestTopRankingTable1(csv)")
+	}
+}
